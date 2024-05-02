6e680ca0098c2e43cef6c2a901c75e7fb4d3f59a
Théo-Gasner <theo.gasner@edu.univ-fcomte.fr>
2024-05-02 09:11:55 +0200

    Ajout ref et vshow

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
   return true
 })
+const menuIsOpen = ref(false)
 </script>
 
 <template>
   <header>
     <nav>
-    <button
+    <button @poinerdown="menuIsOpen = !menuIsOpen"
       aria-controls="mainNav"
       aria-expanded="true"
       class="rounded-full border-2 border-red-600 bg-red-300 px-2"
     >
     Menu
     </button>
-    <nav id="mainNav">
+    <nav id="mainNav " v-show="menuIsOpen">
       <ul>
         <li><a href="#">item 1</a></li>
         <li><a href="#">item 2</a></li>
@@ -39,3 +40,7 @@ onErrorCaptured((err, instance, info) => {
     </Suspense>
   </RouterView>
 </template>
+
+function ref(arg0: boolean) {
+  throw new Error('Function not implemented.')
+}
